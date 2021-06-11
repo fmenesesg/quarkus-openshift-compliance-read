16c5c96f2fb5b1fe544f723c23568f17d5f315b6
Francisco Meneses <fmeneses@redhat.com>
2021-06-11 10:52:07 -0400

    Init

---
+++ src/main/java/com/redhat/ComplianceCheckResult.java
@@ -0,0 +1,54 @@
+package com.redhat;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import io.fabric8.kubernetes.api.model.apiextensions.v1.CustomResourceDefinition;
+import io.fabric8.kubernetes.client.dsl.base.CustomResourceDefinitionContext;
+import io.fabric8.openshift.client.OpenShiftClient;
+import io.vertx.core.json.JsonObject;
+
+import java.util.Map;
+
+import javax.inject.Inject;
+
+import org.jboss.logging.Logger;
+
+
+
+@Path("/ccr")
+public class ComplianceCheckResult {
+
+    private static final Logger LOGGER = Logger.getLogger("ComplianceCheckResult");
+
+    
+    CustomResourceDefinitionContext ccr = new CustomResourceDefinitionContext
+    .Builder()
+    .withGroup("compliance.openshift.io")
+    .withScope("Namespaced")
+    .withVersion("v1alpha1")
+    .withPlural("compliancecheckresults")
+    .build();
+
+    @Inject
+    OpenShiftClient openshiftClient;
+    
+    @GET
+    @Path("/{namespace}")
+    @Produces(MediaType.APPLICATION_JSON)
+    
+    public  JsonObject ccrs(@PathParam("namespace") String namespace) {
+        
+        /*Map<String, Object> response = openshiftClient.customResource(ccr).get();
+        LOGGER.info(response);*/
+        
+        JsonObject jsonObject = new JsonObject(openshiftClient.customResource(ccr).list());
+        LOGGER.info(jsonObject);
+        
+
+        return jsonObject;
+    }
+}
---
+++ src/main/java/com/redhat/OpenShiftSettings.java
@@ -0,0 +1,83 @@
+package com.redhat;
+
+import java.util.Map;
+import static java.util.Map.entry;
+
+import java.util.List;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
+import javax.inject.Inject;
+
+import io.fabric8.kubernetes.api.model.LabelSelector;
+import io.fabric8.kubernetes.api.model.LabelSelectorBuilder;
+import io.fabric8.openshift.api.model.Route;
+import io.fabric8.openshift.client.OpenShiftClient;
+import io.quarkus.runtime.ShutdownEvent;
+import io.quarkus.runtime.StartupEvent;
+
+import org.eclipse.microprofile.config.ConfigProvider;
+import org.jboss.logging.Logger;
+
+@ApplicationScoped
+public class OpenShiftSettings {
+
+    private static final Logger LOGGER = Logger.getLogger("ListenerBean");
+
+    @Inject
+    OpenShiftClient openshiftClient;
+
+    void onStart(@Observes StartupEvent ev) {
+        // Test if we are running in a pod
+        String k8sSvcHost = System.getenv("KUBERNETES_SERVICE_HOST");
+        if (k8sSvcHost == null || "".equals(k8sSvcHost)) {
+            LOGGER.infof("Not running in kubernetes, using CORS_ORIGIN environment '%s' variable",
+                    ConfigProvider.getConfig().getValue("quarkus.http.cors.origins", String.class));
+            return;
+        }
+
+        if (System.getenv("CORS_ORIGIN") != null) {
+            LOGGER.infof("CORS_ORIGIN explicitly defined bypassing route lookup");
+            return;
+        }
+
+        // Look for route with label endpoint:client
+        if (openshiftClient.getMasterUrl() == null) {
+            LOGGER.infof("Kubernetes context is not available");
+        } else {
+            LOGGER.infof("Application is running in OpenShift %s, checking for labelled route",
+                    openshiftClient.getMasterUrl());
+
+            LabelSelector selector = new LabelSelectorBuilder()
+                    .withMatchLabels(Map.ofEntries(entry("endpoint", "client"))).build();
+            List<Route> routes = null;
+            try {
+                routes = openshiftClient.routes().withLabelSelector(selector).list().getItems();
+            } catch (Exception e) {
+                LOGGER.info("Unexpected error occurred retrieving routes, using environment variable CORS_ORIGIN", e);
+                return;
+            }
+            if (routes == null || routes.isEmpty()) {
+                LOGGER.info("No routes found with label 'endpoint:client', using environment variable CORS_ORIGIN");
+                return;
+            } else if (routes.size() > 1) {
+                LOGGER.warn("More then one route found with 'endpoint:client', using first one");
+            }
+
+            Route route = routes.get(0);
+            String host = route.getSpec().getHost();
+            boolean tls = false;
+            if (route.getSpec().getTls() != null && "".equals(route.getSpec().getTls().getTermination())) {
+                tls = true;
+            }
+            String corsOrigin = (tls ? "https" : "http") + "://" + host;
+            System.setProperty("quarkus.http.cors.origins", corsOrigin);
+        }
+        LOGGER.infof("Using host %s for cors origin",
+                ConfigProvider.getConfig().getValue("quarkus.http.cors.origins", String.class));
+    }
+
+    void onStop(@Observes ShutdownEvent ev) {
+        LOGGER.info("The application is stopping...");
+    }
+}
